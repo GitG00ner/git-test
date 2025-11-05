e43f947ec0c6b73c5cd117c6fd37e43a2ada24ef
Filip Andersson <08andfil@taby.se>
2025-11-05 12:56:26 +0100

    hej

---
+++ test-git.py
@@ -0,0 +1,13 @@
+print("instal schoolsoft")
+if "schoolsoft" == "schoolsoft":
+    print("schoolsoft crashade")
+else:
+    print("schoolsoft instalerade rätt?")
+    print("det kan inte stämma!")
+    print("hur?")
+    print("det är någonting konstigt på gång")
+
+print("jag kommer med fler updateringar snart, altså aldrig. Men aja kommer andra förhoppningsvis roliga projekt senare!")
+
+
+print("uppdatering redan nu, jag har stött på problem med att få upp den på github men det löser sig tror jag")
